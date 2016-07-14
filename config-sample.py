9061249334a334d5f34eb7c980730245c9a55194
Francis Tseng <f+accounts@frnsys.com>
2016-07-14 16:07:50 -0400

    config sample

---
+++ config-sample.py
@@ -0,0 +1,5 @@
+DB = 'syllabus.md' # relative to repo
+REPO = '/home/ftseng/projects/darkreadtest'
+REPO_ORIGIN = 'git@github.com:frnsys/testtest.git'
+SLACK_WEBHOOK_URL = 'your slack webhook url'
+LOGFILE='log.py'
